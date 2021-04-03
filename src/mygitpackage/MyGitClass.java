97cec351c3d6f9a418f28e16f4ffc985f43c79fb
yakup2506 <ykpakpinarr@gmail.com>
2021-04-03 19:54:24 +0300

    3.versiyon

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,7 +5,8 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Version 1");
-
+System.out.println("My secon commit");
+System.out.println("3. yorum");
 	}
 
 }
