0f8a8b765dcc5438a4ed12d7707a49523437c5b2
yakup2506 <ykpakpinarr@gmail.com>
2021-04-09 00:17:07 +0300

    deneme

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -3,10 +3,13 @@ package mygitpackage;
 public class MyGitClass {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Version 1");
-System.out.println("My secon commit");
-System.out.println("3. yorum");
+		System.out.println("My secon commit");
+		System.out.println("3. yorum");
+		System.out.println("4.yorum");
+		
+	
 	}
 
 }
