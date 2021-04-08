31757445eefe62b5f617841520ad0766d7bbce27
yakup2506 <81655745+yakup2506@users.noreply.github.com>
2021-04-08 23:31:05 +0300

    Update MyGitClass.java

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -4,9 +4,12 @@ public class MyGitClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Version 1");
-System.out.println("My secon commit");
-System.out.println("3. yorum");
+	System.out.println("Version 1");
+	System.out.println("My secon commit");
+	System.out.println("3. yorum");
+	System.out.println("uzak sunucudaki kodlarim");// git hub daki yeni kodlarim
+		
+		
 	}
 
 }
